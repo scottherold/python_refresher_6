926535220454d8418105e1ec4c63890edd168815
Scott Herold <sherold@mail.usf.edu>
2020-03-03 11:15:01 -0500

    Module/Import basics using turtle

---
+++ turtledemo.py
@@ -0,0 +1,12 @@
+from turtle import forward, right, done, circle
+
+# must load Xming server for this to load the GUI
+
+# turtle instructions
+forward(150)
+right(250)
+forward(150)
+circle(75)
+
+# keeps GUI open for 4 seconds
+done()
