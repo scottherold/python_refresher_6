50fc99813efcd1a20e392c07e052c400224bbb8f
Scott Herold <sherold@mail.usf.edu>
2020-03-12 12:26:15 -0400

    functional programming with refactored functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -14,7 +14,7 @@ def python_food():
 # if a parameter is referenced with a '*', it represents that the
 # parameter can be multiple arguments entered
 # default values can be set as paremeters
-def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+def center_text(*args, sep=' '):
     text = ""
     # converts args into text string
     # the separator is now set as an argument, and is dynamic based on
@@ -23,13 +23,22 @@ def center_text(*args, sep=' ', end='\n', file=None, flush=False):
         text += str(arg) + sep
     left_margin = (80 - len(text) // 2)
     # text variable is sliced to remove final separator
-    print(" " * left_margin, text[:-1], end=end, file=file, flush=flush)
+    return " " * left_margin + text
 
 
+# with open("centered", mode='w') as centered_file:
 # call the function
 # when calling the function, you pass it arguments (as the parameter)
-center_text("spam and eggs")
-center_text("spam, spam and eggs")
-center_text(12)
-center_text("spam, spam, spam and spam")
-center_text("first", "second", 3, 4, "spam", sep=":")
+
+with open("menu", "w") as menu:
+    s1 = center_text("spam and eggs")
+    print(s1, file=menu)
+    s2 = center_text("spam, spam and eggs")
+    print(s2, file=menu)
+    print(center_text(12), file=menu)
+    print(center_text("spam, spam, spam and spam"), file=menu)
+    s5 = center_text("first", "second", 3, 4, "spam", sep=":")
+    print(s5, file=menu)
+
+print("=" + str(12 * 3))
+print(sorted(["b", "d", "c", "a"]))
